76cd7a4aebb00ce9e9ca22d79c5efefbcdb70423
prasun <prasun7483@gmail.com>
2025-07-16 00:05:17 +0530

    final

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Web3-Resources/",
   server: {
     host: "::",
     port: 8080,
